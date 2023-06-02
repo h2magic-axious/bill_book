dd09b9d3a20e82a5e1d514723d9ee6efbcf93ec2
h2magic <soleperson@126.com>
2023-06-02 19:56:04 +0800

    .

--- bill/templatetags/cash_flow.py
+++ bill/templatetags/cash_flow.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter
 def cash_flow(result_list):
-    cash = sum(i.amount for i in result_list)
+    cash = round(sum(i.amount for i in result_list) / 100, 2)
     color = "red" if cash < 0 else "black"
 
     return f"<span style='color: {color}; font-weight: bold'>{cash}</span>"
